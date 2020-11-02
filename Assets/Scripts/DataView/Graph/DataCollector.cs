3be108794c75c827488103ba43b188d2c81dd636
EGKangaroo <EGKangaroo@yahoo.com>
2020-11-02 14:03:24 +0100

    added codemonkey utils and fixed graphs

--- Assets/Scripts/DataView/Graph/DataCollector.cs
+++ Assets/Scripts/DataView/Graph/DataCollector.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,18 +8,41 @@ public class DataCollector : MonoBehaviour
 {
     public UnityEvent DataUpdated = new UnityEvent();
 
-    public int maxHistory = 1000;
+    public int maxHistory = 20;
     public WorldModel data;
 
     List<int> plantsPerStep = new List<int>();
 
+    Dictionary<LifeForm, List<int>> populationBySpecies = new Dictionary<LifeForm, List<int>>();
+
+    private void Start()
+    {
+        LifeForm[] allLifeForms = Resources.LoadAll<LifeForm>("");
+        foreach(var item in allLifeForms)
+        {
+            populationBySpecies.Add(item, new List<int>());
+        }
+    }
+
     public void CollectNewDataPoint()
     {
+        int i = 0;
         List<LifeFormInstance> lifeForms = data.GetAllLifeForms();
-        plantsPerStep.Add(lifeForms.Count);
-        if(plantsPerStep.Count > maxHistory)
+        foreach(var item in populationBySpecies)
         {
-            plantsPerStep.RemoveAt(0);
+            item.Value.Add(0);
+        }
+        foreach(var item in lifeForms)
+        {
+            i = populationBySpecies[item.Species].Count();
+            populationBySpecies[item.Species][i - 1]++;
+        }
+        if(i > maxHistory)
+        {
+            foreach(var item in populationBySpecies)
+            {
+                item.Value.RemoveAt(0);
+            }
         }
         DataUpdated.Invoke();
     }
@@ -26,5 +50,10 @@ public class DataCollector : MonoBehaviour
     public List<int> GetData()
     {
         return plantsPerStep;
+    }
+
+    public Dictionary<LifeForm, List<int>> GetPopulationData()
+    {
+        return populationBySpecies;
     }
 }
--- Assets/Scripts/DataView/Graph/GraphWindow.cs
+++ Assets/Scripts/DataView/Graph/GraphWindow.cs
@@ -13,11 +13,15 @@ public class GraphWindow : MonoBehaviour
     [SerializeField]
     private Sprite node;
 
+    [SerializeField]
     private RectTransform container;
 
-    private void Awake()
+    private int highestDataPoint = 0;
+    private float graphHeight;
+
+    private void Start()
     {
-        container = GetComponent<RectTransform>();
+        graphHeight = container.rect.height;
     }
 
     private void OnEnable()
@@ -27,14 +31,37 @@ public class GraphWindow : MonoBehaviour
 
     public void ShowGraphFromCollector()
     {
-        ShowGraph(collector.GetData());
+        Dictionary<LifeForm, List<int>> pop = collector.GetPopulationData();
+        foreach(List<int> list in pop.Values)
+        {
+            int max = list.Max();
+            if(max > highestDataPoint)
+            {
+                highestDataPoint = max;
+            }
+        }
+
+        foreach (Transform child in container)
+        {
+            if (child.name != "Background")
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        foreach (var item in pop)
+        {
+            ShowGraph(item.Value, item.Key.graphLineColor);
+        }
+        //ShowGraph(collector.GetData());
     }
 
-    void CreateCircle(Vector2 anchoredPos)
+    void CreateCircle(Vector2 anchoredPos, Color color)
     {
         GameObject obj = new GameObject("circle", typeof(Image));
         obj.transform.SetParent(container, false);
         obj.GetComponent<Image>().sprite = node;
+        obj.GetComponent<Image>().color = color;
         RectTransform objRect = obj.GetComponent<RectTransform>();
         objRect.anchoredPosition = anchoredPos;
         objRect.sizeDelta = new Vector2(11f, 11f);
@@ -42,22 +69,18 @@ public class GraphWindow : MonoBehaviour
         objRect.anchorMax = new Vector2(0, 0);
     }
 
-    void ShowGraph(List<int> data)
+    void CreateDotConnection(Vector2 posA, Vector2 posB)
     {
-        //clear graph first
+        GameObject line = new GameObject("DotConnection", typeof(Image));
+        line.transform.SetParent(container);
+
+    }
+
+    void ShowGraph(List<int> data, Color color)
+    {
         if(container != null)
         {
-            foreach (Transform child in container)
-            {
-                if (child.name != "Background")
-                {
-                    Destroy(child.gameObject);
-                }
-            }
-
-            float graphHeight = container.rect.height;
-            int highestpoint = data.Max();
-            int rounder = (int)Mathf.Ceil(highestpoint / 20f);
+            int rounder = (int)Mathf.Ceil(highestDataPoint / 20f);
             float maxY = 20 * rounder;
 
             float xSize = container.rect.width / data.Count;
@@ -66,7 +89,7 @@ public class GraphWindow : MonoBehaviour
             {
                 float xPosition = xSize + i * xSize;
                 float yPosition = (data[i] / maxY) * graphHeight;
-                CreateCircle(new Vector2(xPosition, yPosition));
+                CreateCircle(new Vector2(xPosition, yPosition), color);
             }
         }
     }
